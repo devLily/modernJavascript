11cf9c66db6d9eeb8f7ed28d94f4587b59b96c4e
glorroll <62908757+glorroll@users.noreply.github.com>
2020-08-17 19:04:32 +0900

    spliced

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-// continue 와 breke는 반복문에서 벗어나거나 그 다음 roop를 돌게 할 수 있음
+/*sp는 배열에서 특정 항목을 제거할 때 사용, 그 과정에서 해당 원소가 몇 번째인지 명시해 주어야함
+const numbers = [10, 20, 30, 40];
+const index = numbers.indexOf(30);
+//만약 index가 2인 것을 배열에서 제외시키고 싶다면
+numbers.splice(index, 2);
+// 2를 입력하면 index부터 시작해서 2개를 지우겠다 라는 의미
+console.log(numbers);*/
 
-for (let i = 0; i < 10; i++) {
-  if (i === 2) {
-    continue;
-  }
-  console.log(i);
-  if (i === 5) break;
-}
-
-// continue를 사용했을 때 console의 값에서 2가 사라짐. 이는 continue에서 주어진 조건을 만족 했을 때 다음 roop로 넘어간다는 의미
-// if문 내부에서 실행할 코드가 한 줄 이라면 {} 생략 갸능
-//if (i === 2) continue;
-
-// break는 반복문을 끝낼 수 있음
-// continue와 break는 for문 뿐아니라 while문, for..of, for..in문에서도 사용할 수 있다.
+//numbers.splice의 결과 값은 제거한 배열들을 의미
+const numbers = [10, 20, 30, 40];
+const index = numbers.indexOf(30);
+const spliced = numbers.splice(index, 2);
+console.log(spliced);
+console.log(numbers);
