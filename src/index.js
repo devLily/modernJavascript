d985ec0377f07f786ce3578078b0a49a40eba71e
glorroll <62908757+glorroll@users.noreply.github.com>
2020-08-17 21:50:19 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,34 +1,9 @@
-//filter 특정 조건을 만족하는 원소를 찾아 새로운 배열을 만드는 것
-const todos = [
-  {
-    id: 1,
-    text: "자바스크립트 입문",
-    done: true
-  },
-  {
-    id: 2,
-    text: "함수 배우기",
-    done: true
-  },
-  {
-    id: 3,
-    text: "객체와 배열 배우기",
-    done: true
-  },
-  {
-    id: 4,
-    text: "배열 내장함수 배우기",
-    done: false
-  }
-];
-// todo 배열에서 done 값이 false 인 것만 필터링 하려면
+// shift는 첫 번째 원소를 배열에서 추출해 줌
+const numbers = [10, 20, 30, 40];
 
-const tasksNotDone = todos.filter((todo) => todo.done === false);
-console.log(tasksNotDone);
+const value = numbers.shift();
+console.log(value);
+// numbers 배열에서 10이 shift를 통해 밖으로 빠져나오면서 value안으로 들어감
 
-/* filter내장함수를 사용하여 tasksNotDone이라는 배열을 만들고 특정 조건을 넣어주고 이는 함수로 확인한다.
-todo라는 파라미터를 가져와서 todo.done 값이 false인 배열을 만들어 내는 것
-기존의 배열을 건드리지 않고 새로운 배열을 만들어 낸다.
- false 값을 더 간략하게 작성하기 위해 
-const tasksNotDone = todos.filter((todo) => !todo.done);
-와 같이 작성 할 수 있다. */
+// shift는 기존의 배열을 수정하게 됨
+console.log(numbers);
