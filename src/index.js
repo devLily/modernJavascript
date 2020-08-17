d7ac2768701d14293b22b8a85ebeabc02e3a2124
glorroll <62908757+glorroll@users.noreply.github.com>
2020-08-17 21:23:49 +0900

    slice

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-/*sp는 배열에서 특정 항목을 제거할 때 사용, 그 과정에서 해당 원소가 몇 번째인지 명시해 주어야함
+//slice는 배열을 찾아 낼 때 사용하며 splice와의 차이점은 기존의 배열을 건드리지 않는다, 파라미터에 넣는 값도 다름
 const numbers = [10, 20, 30, 40];
-const index = numbers.indexOf(30);
-//만약 index가 2인 것을 배열에서 제외시키고 싶다면
-numbers.splice(index, 2);
-// 2를 입력하면 index부터 시작해서 2개를 지우겠다 라는 의미
-console.log(numbers);*/
 
-//numbers.splice의 결과 값은 제거한 배열들을 의미
-const numbers = [10, 20, 30, 40];
-const index = numbers.indexOf(30);
-const spliced = numbers.splice(index, 2);
-console.log(spliced);
+const sliced = numbers.slice(0, 2);
+console.log(sliced);
 console.log(numbers);
+//start라는 파라미터는 어떤 index부터 자를지를 의미, end파라미터는 어디까지를 자를지 의미
+//0의 index는 10, 2의 index는 30이므로 10, 20을 잘라내고 sliced의 console값이 [10, 20]이 됨
+// 강조 : slice 함수는 기존의 배열을 건드리지 않는다. 반면 splice는 기존의 배열을 수정한다는 점에서 차이가 있음
